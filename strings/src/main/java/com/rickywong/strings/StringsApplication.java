2952b0aebbfb599c53747be43c4d907d44e39367
wongricky <50374597+wongricky@users.noreply.github.com>
2019-08-11 14:45:56 -0500

    Add files via upload

---
+++ strings/src/main/java/com/rickywong/strings/StringsApplication.java
@@ -0,0 +1,23 @@
+package com.rickywong.strings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@SpringBootApplication
+@RestController
+public class StringsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringsApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+	public String main() {
+		return "Hello client! How are you doing?";
+	}
+	
+
+}
